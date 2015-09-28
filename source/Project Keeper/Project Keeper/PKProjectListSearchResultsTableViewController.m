abe313dba023d4d66b72e9c5eb56b18685910e63
Mary Zelenska <marina.zelenskaya@gmail.com>
2015-09-28 10:32:23 +0300

    Search option was added

---
+++ source/Project Keeper/Project Keeper/PKProjectListSearchResultsTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  PKProjectListSearchResultsTableViewController.m
+//  Project Keeper
+//
+//  Created by Mary Zelenska on 9/25/15.
+//  Copyright (c) 2015 3ss. All rights reserved.
+//
+
+#import "PKProjectListSearchResultsTableViewController.h"
+
+@interface PKProjectListSearchResultsTableViewController ()
+
+@end
+
+@implementation PKProjectListSearchResultsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
